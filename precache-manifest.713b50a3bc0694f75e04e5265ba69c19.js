a4313a420b52bb8afb803378286c4869d2a9e824
Doris <d1882002@gmail.com>
2020-07-24 15:22:55 +0800

    Updates

---
+++ precache-manifest.713b50a3bc0694f75e04e5265ba69c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90603453695c5791f89f7f6c81ecb9c4",
+    "url": "/react-random-user/index.html"
+  },
+  {
+    "revision": "7dfbe82b0d690578e293",
+    "url": "/react-random-user/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "beed4dd0493b1da32419",
+    "url": "/react-random-user/static/css/main.e8dbed62.chunk.css"
+  },
+  {
+    "revision": "7dfbe82b0d690578e293",
+    "url": "/react-random-user/static/js/2.7a579568.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-random-user/static/js/2.7a579568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beed4dd0493b1da32419",
+    "url": "/react-random-user/static/js/main.b60bc784.chunk.js"
+  },
+  {
+    "revision": "fdce94f6ffcdc231d153",
+    "url": "/react-random-user/static/js/runtime-main.758b98ce.js"
+  }
+]);
